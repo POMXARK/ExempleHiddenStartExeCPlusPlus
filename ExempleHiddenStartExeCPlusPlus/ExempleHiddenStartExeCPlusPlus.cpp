ceeaecd8ea88edd371004bdd6afc07208fc58107
POMXARK <1997pom@gmail.com>
2022-05-17 12:53:58 +1000

    Добавьте файлы проекта.

---
+++ ExempleHiddenStartExeCPlusPlus/ExempleHiddenStartExeCPlusPlus.cpp
@@ -0,0 +1,20 @@
+﻿// ExempleHiddenStartExeCPlusPlus.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h> 
+#include <stdio.h> 
+
+int main()
+{
+    HINSTANCE load;
+    load = LoadLibrary(L"HiddenStartExe.dll");
+    typedef void (*hidden_start) (const char*);
+    hidden_start HiddenStart;
+    HiddenStart = (hidden_start)GetProcAddress(load, "HiddenStart");
+    const char*  prog_name = "main.exe";
+    HiddenStart(prog_name);
+    std::cout << "Hello World!\n";
+    FreeLibrary(load);
+}
+
